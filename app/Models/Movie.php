9a12a51b431494101bb322d381c455900ed0e9a7
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-06-09 15:43:56 +0200

    completo

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -5,6 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @mixin Builder
+ * @property int id
+ * @property string title
+ * @property string original_title
+ * @property string nationality
+ * @property date date
+ * @property float vote
+ * 
+ */
 class Movie extends Model
 {
     use HasFactory;
