b8557dfc105c8ab2bdd0ff9f30548ed636bb8c52
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-06-09 15:25:08 +0200

    struttura 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,21 @@
 </head>
 
 <body>
-
+    <div class="container">
+        <div class="row gap-5">
+            @foreach ($movies as $item)
+            <div class="card col-3">
+                <div class="card-body">
+                  <h5 class="card-title">{{$item['title']}}</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">{{$item['title']}}</h6>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="card-link">Card link</a>
+                  <a href="#" class="card-link">Another link</a>
+                </div>
+              </div>
+            @endforeach
+        </div>
+    </div>
     <h1>
         ciao
     </h1>
